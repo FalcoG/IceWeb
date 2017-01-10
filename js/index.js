abd46b816fcf61cf050ba06d08a210e9e4b41f4c
Falco <falco@gerritsjans.com>
2017-01-10 15:51:17 +0100

    Check if Ice Poseidon is live, When offline check if banned, Add LIVE label for Twitch logo, Remove jQuery and convert to pure JavaScript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-var FebFirst = new Date(2017, 01, 01, 0, 0, 0, 0)
-var unban_timer = $(".unban-time").first()
+var config = {
+    api_key: 'i249p26eo0882g7svswi1won5zkeo8'
+};
 
 var formatTime = function(seconds) {
 	// get total seconds between the times
@@ -21,11 +22,77 @@ var formatTime = function(seconds) {
 	var seconds = delta % 60;  // in theory the modulus is not required
 
 	return (days + "d:" + hours + "h:" + minutes + "m:" + seconds.toString().split(".")[0] + "s")
+};
+
+var status_elem = document.getElementById('status');
+
+function twitch_status(username){
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                try{
+                    var response = JSON.parse(xhr.responseText);
+
+                    // null = not live
+                    if(response.stream == null){
+                        status_elem.innerHTML = '<p class="col-12">Ice Poseidon is not live</p>';
+                        ban_check(username);
+                    }else{
+                        status_elem.innerHTML = '<p class="col-12">Ice Poseidon is live</p>';
+                        document.getElementsByClassName('social-button twitch')[0].className += ' live';
+                    }
+                }catch(err){
+                    // something went wrong
+                }
+
+            }
+        };
+        xhr.open('GET', 'https://api.twitch.tv/kraken/streams/' + username, true);
+        xhr.setRequestHeader('Client-ID', config.api_key);
+        xhr.send(null);
+    }catch(err){
+        console.log('Twitch API is unavailable');
+    }
 }
 
-unban_timer.text(formatTime(FebFirst - $.now()))
+function ban_check(username){
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                var response = JSON.parse(xhr.responseText);
 
-setInterval(function() {
-	unban_timer.text(formatTime(FebFirst - $.now()))
-}, 1000);
+                if(response.error){
+                    // status code 422 means he's banned
+                    if(response.status == 422){
+                        var unban_date = new Date(2017, 1, 1, 0, 0, 0, 0);
 
+                        if(unban_date > Date.now()){ // if latest known unban date is
+                            status_elem.innerHTML = '<p class="col-12">Ice Poseidon will be unbanned in</p><p class="unban-time col-12">' + formatTime(unban_date - Date.now()) + '</p>';
+                            setInterval(function(){
+                                status_elem.childNodes[1].innerHTML = formatTime(unban_date - Date.now());
+                            }, 1000);
+                        }else{
+                            if(unban_date > Date.now() - 86400000){ // 1 day in milliseconds
+                                
+                            }else{
+                                // last known unban date is greater than 1 day
+                                status_elem.innerHTML = '<p class="col-12">Ice Poseidon is possibly banned</p>';
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        xhr.open('GET', 'https://api.twitch.tv/kraken/channels/' + username, true);
+        xhr.setRequestHeader('Client-ID', config.api_key);
+        xhr.send(null);
+    }catch(err){
+        console.log('Twitch API is unavailable');
+    }
+
+}
+
+twitch_status('ice_poseidon');
